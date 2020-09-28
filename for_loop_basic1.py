d37947dfbf4a832f746a94d844294375908027df
Ling Xu <36904516+xulingggg@users.noreply.github.com>
2020-09-28 15:46:24 -0700

    Add files via upload

---
+++ for_loop_basic1.py
@@ -0,0 +1,38 @@
+#1
+#for i in range (0, 151, 1):
+    #print(i)
+#for i in range (151):
+    #print(i)
+
+#2
+#for i in range(5, 1001, 5):
+    #print(i)
+
+#3
+# for i in range(1,101,1):
+    #if i % 10 == 0:
+        #print('Coding Dojo')
+    #elif i % 5 == 0:
+        #print('Coding')
+    #else:
+        #print(i)
+
+#4
+#var_sum = 0
+#for i in range(1, 500001, 2):  
+    #var_sum += i
+#print(var_sum)
+
+#5
+#for i in range (2018, 0, -4):
+    #print(i)
+
+#6
+#lowNum = 2
+#highNum = 9
+#mult = 3
+#for i in range(lowNum, highNum+1, 1):
+    #if i % mult == 0:
+        #print(i)
+
+
