0ec8773a34a77f8a7c30e9286c845f5ba3985099
Ling Xu <36904516+xulingggg@users.noreply.github.com>
2020-09-28 15:15:54 -0700

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,25 @@
+print('Hello World')
+
+name = "Ling"
+num = 888
+favFood1 = "shrimp"
+favFood2 = "lollypop"
+
+print('Hello', name)
+
+print('hello '+ name)
+
+print('hello', num)
+
+print('hello ' + str(num))
+
+print("I love to eat {} and {}.".format(favFood1, favFood2))
+
+print(f"I love to eat {favFood1} and {favFood2}.")
+
+
+
+
+
+
+
